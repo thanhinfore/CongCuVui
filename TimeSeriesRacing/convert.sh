5ea45d8b18a8022b09105fdc3c90198736a231ef
thanhinfore <thanh@infore.vn>
2025-10-29 20:57:10 +0700

    123

---
+++ TimeSeriesRacing/convert.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+python TimeSeriesRacing.py examples/sports_data/23_man_utd_vs_brighton_match_activity.csv --title "⚽ Manchester United vs Brighton - Activity Race" 
+  --palette fire 
+  --bar-border-width 4.0 
+  --bar-alpha 0.97 
+  --fps 60 
+  --dpi 200 
+  --preset tiktok
